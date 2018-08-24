575af3d4d167be8796421820415760637c865c04
daniel.torres <daniel.torres@SQEST103.squadra.com.br>
2018-08-24 15:02:10 -0300

    conexao com banco

---
+++ Gerenciador de Arquivos/src/Connection/connectDB.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+
+public class connectDB {
+	
+	Connection conn = null;
+
+	public static Connection CriarConexao() {
+
+		// Configuração dos parâmetros de conexão
+        String server = "192.168.0.41";
+        String port = "1521";  // Porta TCP padrão do Oracle
+        String database = "uganda.nosmoke";
+
+        // Configuração dos parâmetros de autenticação
+        String user = "req";
+        String passwd = "req";
+
+        try {
+            String url = "jdbc:oracle:thin:@" + server + ":" + port + ":" + database;
+
+            // Abre-se a conexão com o Banco de Dados
+            conn = DriverManager.getConnection(url, user, passwd);
+        } catch (Exception e) {
+			e.printStackTrace();
+			System.out.print("Erro de conexão com o banco de dados.");
+		}
+        return conn;
+	}
+	
+	public void FecharConexao() {
+		conn.close();
+	}
+
+}
