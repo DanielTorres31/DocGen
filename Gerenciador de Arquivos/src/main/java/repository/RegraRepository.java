bd7c496c88d1eab5a4b2ebb2f86fc8a424393bc6
daniel.torres <daniel.torres@SQEST103.squadra.com.br>
2018-08-27 12:21:18 -0300

    Metodos findAll

--- Gerenciador de Arquivos/src/main/java/repository/RegraRepository.java
+++ Gerenciador de Arquivos/src/main/java/repository/RegraRepository.java
@@ -3,6 +3,9 @@ package repository;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import entities.Regra;
 
@@ -10,8 +13,14 @@ public class RegraRepository {
 	EntityManager em;
 	
 	public List<Regra> findAll() {
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Regra> query = builder.createQuery(Regra.class);
+		query.from(Regra.class);
+		TypedQuery<Regra> typedQuery = em.createQuery(query);
 		
-		return null;
+		List<Regra> regras = typedQuery.getResultList();
+		
+		return regras;
 	}
 
 }
--- Gerenciador de Arquivos/src/main/java/repository/TipoRegrasRepository.java
+++ Gerenciador de Arquivos/src/main/java/repository/TipoRegrasRepository.java
@@ -1,5 +1,27 @@
 package repository;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
+import entities.TipoRegra;
+
 public class TipoRegrasRepository {
+	EntityManager em;
+	
+	public List<TipoRegra> findAll() {
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<TipoRegra> query = builder.createQuery(TipoRegra.class);
+		query.from(TipoRegra.class);
+		TypedQuery<TipoRegra> typedQuery = em.createQuery(query);
+		
+		List<TipoRegra> tipoRegras = typedQuery.getResultList();
+		
+		return tipoRegras;
+	}
+	
 
 }
